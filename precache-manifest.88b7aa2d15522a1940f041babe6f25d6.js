1d4da7825dd970303daf17019aa8632b3e176745
Iury Vieira Lippo da Silva <iuryvieira.lippo@gmail.com>
2019-10-13 22:02:19 -0300

    Updates

---
+++ precache-manifest.88b7aa2d15522a1940f041babe6f25d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90dcb0e2347ebba1d9261949aa4224d4",
+    "url": "/react-dice-game/index.html"
+  },
+  {
+    "revision": "8954ced904122bd67212",
+    "url": "/react-dice-game/static/css/main.571d80e1.chunk.css"
+  },
+  {
+    "revision": "9ab091a64246cddd1d90",
+    "url": "/react-dice-game/static/js/2.786a1a76.chunk.js"
+  },
+  {
+    "revision": "8954ced904122bd67212",
+    "url": "/react-dice-game/static/js/main.24529120.chunk.js"
+  },
+  {
+    "revision": "db5636bfd79811b4bc44",
+    "url": "/react-dice-game/static/js/runtime-main.0862f36f.js"
+  }
+]);
